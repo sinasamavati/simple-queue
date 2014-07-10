49cff45a8951378605f1657b435ab2ad06ad919b
Sina Samavati <sina.samv@gmail.com>
2014-07-11 00:18:12 +0430

    Change commands - print more stuff to the console

--- src/sq_server.erl
+++ src/sq_server.erl
@@ -12,20 +12,23 @@ start_link(Listen) ->
                     end)}.
 
 %% commands:
-%%   j -> join
-%%   r -> route
+%%   1 -> join
+%%   2 -> route
+%%
+%% the first segment is command, the second one is channel name which should be
+%%   4 bytes, the third segment is any other data
 loop(Sock) ->
     receive
-        {tcp, Sock, <<$j, Chan:4/binary, _/binary>>} ->
+        {tcp, Sock, <<1, Chan:4/binary, _/binary>>} ->
             sq_state:join_chan(Sock, Chan),
             %% gen_tcp:send(Sock, <<1>>),
             inet:setopts(Sock, [{active, once}]),
-            io:format("join ~p~n", [Chan]),
+            io:format("~p joined ~s~n", [Sock, Chan]),
             loop(Sock);
-        {tcp, Sock, <<$r, Chan:4/binary, Data/binary>>} ->
+        {tcp, Sock, <<2, Chan:4/binary, Data/binary>>} ->
             [gen_tcp:send(S, Data) || S <- sq_state:get_members(Chan)],
             inet:setopts(Sock, [{active, once}]),
-            io:format("sent data to ~p~n", [Chan]),
+            io:format("~p sent data to the channel ~s~n", [Sock, Chan]),
             loop(Sock);
         {tcp_closed, Socket} ->
             io:format("socket closed!~n"),
